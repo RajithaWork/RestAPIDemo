b6172bba7e99273fa866690506d33bb965384758
Rajitha <testingrajithawork@gmail.com>
2024-08-01 19:42:50 +0530

    first commit

---
+++ src/demopac/Basic.java
@@ -0,0 +1,11 @@
+package demopac;
+
+public class Basic {
+
+	public static void main(String[] args) {
+
+		System.out.println("Kannaya is a good boy");
+		System.out.println("Ramesh is a Old bad  Men");
+	}
+
+}
